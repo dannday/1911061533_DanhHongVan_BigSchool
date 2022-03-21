b4cfa21729620cf2a4cc3425eb614f4b251f81bd
Hong Van <danhhongvan874@gmail.com>
2022-03-21 08:45:42 +0700

    commit and pull ngay 21/03

---
+++ 1911061533_DanhHongVan_BigSchool/Models/Course.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _1911061533_DanhHongVan_BigSchool.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+
+        public ApplicationUser Lecturer { get; set;
+        [Required]
+        public string LecturerId { get; set; }
+        [Required]
+        [StringLength(255)]
+
+
+    }
+    }
+}
