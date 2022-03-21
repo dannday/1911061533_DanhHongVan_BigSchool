3392b8799ecb1d778a86a81d54dc3c854c736896
Hong Van <danhhongvan874@gmail.com>
2022-03-21 11:08:35 +0700

    Commit 11h ngay 21/3

---
+++ 1911061533_DanhHongVan_BigSchool/Migrations/202203210224107_CretaTableCourse.cs
@@ -0,0 +1,16 @@
+namespace _1911061533_DanhHongVan_BigSchool.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CretaTableCourse : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
